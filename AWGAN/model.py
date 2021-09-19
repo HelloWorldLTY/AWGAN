67de0d06505d5a73ff0ca175568f50095e6510c3
ChineseBest <43333475+ChineseBest@users.noreply.github.com>
2021-09-18 21:33:47 -0500

    version2

--- AWGAN/model.py
+++ AWGAN/model.py
@@ -173,7 +173,7 @@ def determine_batch(val1):
       continue
   return val1
 
-def WGAN_train(train_label,train_data,epoch,batch,lambda_1):
+def WGAN_train_type1(train_label,train_data,epoch,batch,lambda_1):
       stop = 0
   iter = 0
   D = discriminator()
@@ -193,9 +193,6 @@ def WGAN_train(train_label,train_data,epoch,batch,lambda_1):
   MAX_ITER = train_data.shape[0]
   batch = determine_batch(MAX_ITER)
 
-    
-  # scheduler_D =torch.optim.lr_scheduler.OneCycleLR(d_optimizer,max_lr=lr,total_steps=(epoch)*(MAX_ITER//batch+1), verbose=False)
-  # scheduler_G =torch.optim.lr_scheduler.OneCycleLR(g_optimizer,max_lr=lr,total_steps=(epoch)*(MAX_ITER//batch+1), verbose=False) 
   for epoch_1 in range(epoch):
     print("This is ", epoch_1)
     for time in range(0,MAX_ITER,batch):
@@ -250,14 +247,91 @@ def WGAN_train(train_label,train_data,epoch,batch,lambda_1):
   test_list = G(test_data).detach().cpu().numpy()    
   return test_list,G
 
-def sequencing_train(ref_adata, batch_adata, batch_inf, epoch=100, batch=32, lambda_1=1/10):
+def WGAN_train_type2(train_label,train_data,epoch,batch,lambda_1):
+      stop = 0
+  iter = 0
+  D = discriminator()
+  G = generator()
+
+  if torch.cuda.is_available():
+    D = D.cuda()
+    G = G.cuda()
+  lr=0.0001
+
+  d_optimizer = torch.optim.AdamW(D.parameters(), lr=lr)
+  g_optimizer = torch.optim.AdamW(G.parameters(), lr=lr)  
+
+  G.train()
+  D.train()
+
+  MAX_ITER = train_data.shape[0]
+  batch = determine_batch(MAX_ITER)
+
+  for epoch_1 in range(epoch):
+    print("This is ", epoch_1)
+    for time in range(0,MAX_ITER,batch):
+      true_data = torch.FloatTensor(train_label[time:time+batch,:]).cuda()
+      false_data = torch.FloatTensor(train_data[time:time+batch,:]).cuda()
+    
+
+      #train d at first
+
+      d_optimizer.zero_grad()
+
+      real_out = D(true_data)
+      real_label_loss = -torch.mean(real_out)
+
+      # err_D.append(real_label_loss.cpu().float())
+
+      # train use WGAN
+
+      fake_out_new = G(false_data).detach()
+      fake_out = D(fake_out_new)
+
+      div = calculate_gradient_penalty(true_data, fake_out_new, D)
+
+      label_loss = real_label_loss+torch.mean(fake_out)+div/lambda_1
+      label_loss.backward()
+
+      # err_D.append(label_loss.cpu().item())
+
+      d_optimizer.step()
+      # scheduler_D.step()
+  
+      #train G
+
+      real_out = G(false_data)
+      real_output = D(real_out)
+
+      real_loss1 = -torch.mean(real_output)
+      # err_G.append(real_loss1.cpu().item())
+
+      g_optimizer.zero_grad()
+
+      real_loss1.backward()
+      g_optimizer.step()
+      # scheduler_G.step()
+
+      if(time%100==0):
+        print("g step loss",real_loss1)
+      iter += 1
+  print("Train step finished")
+  G.eval()
+  test_data = torch.FloatTensor(train_data).cuda()
+  test_list = G(test_data).detach().cpu().numpy()    
+  return test_list,G
+
+def sequencing_train(ref_adata, batch_adata, batch_inf, epoch=100, batch=32, lambda_1=1/10, type_key=1):
       ref_data_ori = ref_adata.X
   for bat_inf in batch_inf[1:]:
     print("##########################Training%s#####################"%(bat_inf))
     batch_data_ori = batch_adata[batch_adata.obs['batch'] == bat_inf].X
     label_data,train_data = training_set_generator(ref_data_ori, batch_data_ori, ref_data_ori, batch_data_ori)
     print("#################Finish Pair finding##########################")
-    remove_batch_data,G_tar = WGAN_train(label_data,train_data,epoch,batch,lambda_1)
+    if type_key==1:
+      remove_batch_data,G_tar = WGAN_train_type1(label_data,train_data,epoch,batch,lambda_1)
+    else:
+      remove_batch_data,G_tar = WGAN_train_type2(label_data,train_data,epoch,batch,lambda_1)   
     ref_data_ori = np.vstack([ref_data_ori,remove_batch_data])
   print("###################### Finish Training ###########################")
   return ref_data_ori, G_tar
